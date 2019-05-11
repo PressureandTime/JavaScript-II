7cd8749a542cd10e5d32798e03e72e08f6b49570
Petar Scepanovic <hiborija@gmail.com>
2019-05-11 13:37:57 +0200

    started working on callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,23 +1,25 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. 
+//You have been provided an example of a problem and a solution to see how this works with our items array.
+//  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
 
-  //Given this problem: 
+  Given this problem: 
   
   function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
+    firstItem passes the first item of the given array to the callback function.
   }
 
-  // Potential Solution:
+  Potential Solution:
 
-  // Higher order function using "cb" as the call back
+  Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
 
-  // Function invocation 
+  Function invocation 
   firstItem(items, function(first) {
     console.log(first)
   });
@@ -25,13 +27,35 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
-function getLength(arr, cb) {
+function getLength(arr, callback) {
   // getLength passes the length of the array into the callback.
+  return callback(arr);
 }
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+function length(arr){
+  const alfa = console.log(arr.length);
+  return alfa;  
 }
+
+getLength(items, length);
+
+
+/////////////////////////////////////////
+
+function last(arr, callback) {
+  // last passes the last item of the array into the callback.
+  return callback(arr);
+}
+
+function lastItem(arr){
+  const alfa = console.log(arr[3]);
+  return alfa;
+}
+
+last(items,lastItem);
+
+
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
