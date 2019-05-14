b49ccc9ed2cb8e75cb6967464fc7a7c0dba22453
Petar Scepanovic <hiborija@gmail.com>
2019-05-14 14:03:58 +0200

    cant figure out stretch problem, challenge 3

--- assignments/closure.js
+++ assignments/closure.js
@@ -45,7 +45,28 @@ console.log(doubler(100));
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+
+  let alfa = 2;
+  let someObject = {
+    increment: function () {
+      ++alfa;
+      console.log(alfa);
+    },
+
+    decrement: function () {
+      --alfa;
+      console.log(alfa);
+    }
+  };
+
+  return someObject;
+
 };
+
+let bla = counterFactory();
+bla.increment();
+
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.    let test = counter();
+// test.increment();
