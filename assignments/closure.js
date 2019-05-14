632ff51a1a3de957e587de0272e6707f7d49baa4
Petar Scepanovic <hiborija@gmail.com>
2019-05-14 13:27:25 +0200

    finished one stretch goal in closure.js, moving to next one

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,36 +1,47 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+// const alfa = "WHATS UP";
 
-const alfa = "WHATS UP";
+// function myFunction() {
+//   console.log(alfa);
+//   const beta = "NOTHING,MUCH";
 
-function myFunction() {
-  console.log(alfa);
-  const beta = "NOTHING,MUCH";
-
-  function nestedFunction() {
-    console.log(beta);
-    const gama = "ALL RIGHT THEN"
-    function nestedAnotherOne() {
-      const delta = 'BYE';
-    }
-  }
-  nestedFunction();
-}
-myFunction();
-
-
+//   function nestedFunction() {
+//     console.log(beta);
+//     const gama = "ALL RIGHT THEN";
+//     function nestedAnotherOne() {
+//       const delta = "BYE";
+//     }
+//   }
+//   nestedFunction();
+// }
+// myFunction();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
-
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+// Return a function that when invoked increments and returns a counter variable.
+
+
+function multiplier(factor) {
+  debugger;
+  return function (x) {
+    debugger;
+    return x * factor;
+  };
+}
+
+let doubler = multiplier(100);
+
+console.log(doubler(100));
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
